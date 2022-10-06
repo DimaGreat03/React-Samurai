2d115859c81df523040e5b75fb3af63b2441cb43
DmitryOntov <Ontoev2016@gmail.com>
2022-10-06 13:50:59 +0800

    LESSON 09. Учусь разносить JSX на компоненты и с помощью импорта и экспорта использовать их в APP.JS

--- 01-first-project/react-learning/src/App.js
+++ 01-first-project/react-learning/src/App.js
@@ -1,5 +1,7 @@
-import logo from './logo.svg';
 import './App.css';
+import Technologies from "./Technologies";
+import Header from "./Header";
+
 
 const App = (props) => {
     return (
@@ -9,38 +11,6 @@ const App = (props) => {
         </div>
     );
 }
-
-const Header = (props) => {
-    return (
-        <div>
-            <p><a href="#">Power</a></p>
-            <p><a href="#">Education</a></p>
-            <p><a href="#">Just Keep</a></p>
-            <p><a href="#">Funny</a></p>
-        </div>
-    );
-}
-    const Technologies = (props) => {
-        return (
-            <div className="App">
-                <ul>
-                    <p>
-                        <li>I AM LEARNING REACT. NOW 06.10.2022.</li>
-                    </p>
-                    <p>
-                        <li>SECOND LEARNING</li>
-                    </p>
-                    <p>
-                        <li>JUST KEEP LEARNING</li>
-                    </p>
-                    <p>
-                        <li>DONT STOP</li>
-                    </p>
-                </ul>
-                <div> DONT STOP DIMA</div>
-            </div>
-        )
-    }
 
 
 export default App;
---
+++ 01-first-project/react-learning/src/Header.js
@@ -0,0 +1,12 @@
+const Header = (props) => {
+    return (
+        <div>
+            <p><a href="#">Power</a></p>
+            <p><a href="#">Education</a></p>
+            <p><a href="#">Just Keep</a></p>
+            <p><a href="#">Funny</a></p>
+        </div>
+    );
+}
+
+export default Header
