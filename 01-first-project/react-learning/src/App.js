b5b9e888f55c02892d5baa92f2f166e1203c012e
DmitryOntov <Ontoev2016@gmail.com>
2022-10-06 13:19:04 +0800

    LESSON 7. Начал изучать основы компоненты, то есть дробление на элементы

--- 01-first-project/react-learning/src/App.js
+++ 01-first-project/react-learning/src/App.js
@@ -4,25 +4,44 @@ import './App.css';
 const App = (props) => {
     return (
         <div>
-        <div> DONT STOP DIMA</div>
-        <div className="App">
-            <ul>
-                <p>
-                    <li>I AM LEARNING REACT. NOW 06.10.2022.</li>
-                </p>
-                <p>
-                    <li>SECOND LEARNING</li>
-                </p>
-                <p>
-                    <li>JUST KEEP LEARNING</li>
-                </p>
-                <p>
-                    <li>DONT STOP</li>
-                </p>
-            </ul>
-        </div>
+            <Header/>
+            <Technologies/>
         </div>
     );
 }
 
+const Header = (props) => {
+    return (
+        <div>
+            <p><a href="#">Power</a></p>
+            <p><a href="#">Education</a></p>
+            <p><a href="#">Just Keep</a></p>
+            <p><a href="#">Funny</a></p>
+        </div>
+    );
+}
+    const Technologies = (props) => {
+        return (
+            <div className="App">
+                <ul>
+                    <p>
+                        <li>I AM LEARNING REACT. NOW 06.10.2022.</li>
+                    </p>
+                    <p>
+                        <li>SECOND LEARNING</li>
+                    </p>
+                    <p>
+                        <li>JUST KEEP LEARNING</li>
+                    </p>
+                    <p>
+                        <li>DONT STOP</li>
+                    </p>
+                </ul>
+                <div> DONT STOP DIMA</div>
+            </div>
+        )
+    }
+
+
 export default App;
+
