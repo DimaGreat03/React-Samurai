ff68442b6e655b38312505062c64c6b2ef416ce4
DmitryOntov <Ontoev2016@gmail.com>
2022-10-06 12:46:47 +0800

    05 LESSON. изучаю основы ректа по второму кругу.

---
+++ 01-first-project/react-learning/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+const App = (props) => {
+    return (
+        <div>
+        <div> DONT STOP DIMA</div>
+        <div className="App">
+            <ul>
+                <p>
+                    <li>I AM LEARNING REACT. NOW 06.10.2022.</li>
+                </p>
+                <p>
+                    <li>SECOND LEARNING</li>
+                </p>
+                <p>
+                    <li>JUST KEEP LEARNING</li>
+                </p>
+                <p>
+                    <li>DONT STOP</li>
+                </p>
+            </ul>
+        </div>
+        </div>
+    );
+}
+
+export default App;
